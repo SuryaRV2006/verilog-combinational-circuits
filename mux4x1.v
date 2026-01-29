aa89a172fa5530698f9a9c85a97cdc3210c3d16b
SuryaRV2006 <surya07rv@gmail.com>
2026-01-29 21:51:31 +0530

    delete mux4x1 module

--- mux4x1.v
+++ mux4x1.v
@@ -1,7 +1,1 @@
-module mux_4x1(Y,I,S);
-  input[3:0]I;
-  input[1:0]S;
-  output Y;
-  assign Y=(S==2'd0)?I[0]:(S==2'd1)?I[1]:(S==2'd2)?I[2]:I[3];
-endmodule
-                                                    
+
